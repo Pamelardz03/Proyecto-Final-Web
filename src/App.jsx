adbd7547821825434cafd8bd1618ce51d2db5302
Pamela Rdz <pamela.rodriguezd@udem.edu>
2025-11-22 16:58:41 -0600

    Guardar archivo que faltaba

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-import Home from "./pages/paginaprincipal";
 import PaginaPrincipal from './pages/paginaprincipal';
 
 function App() {
   return (
-    <div>
-      <PaginaPrincipal />   {}
-    </div>
+      <PaginaPrincipal />
   );
 }
-
 
 export default App
